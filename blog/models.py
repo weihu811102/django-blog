2f16695644181bf24d6f3fbe746f9cce7616293a
WeiHu <weihu120@gmail.com>
2014-09-14 18:32:32 +0800

    blog models defined.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __unicode__(self):
+        return self.name
+
+class Blog(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add = True)
+    category = models.ManyToManyField(Category)
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        ordering=["-timestamp", "title"]
