7ea91ce011872a2d3a14e0f324156155487efe42
WeiHu <weihu120@gmail.com>
2014-09-14 18:45:39 +0800

    model admin added.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from models import Blog, Category
 
 # Register your models here.
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ("title", "timestamp")
+    list_filter = ("timestamp",)
+    search_fields = ("title", "content")
+    ordering = ("-timestamp", "title")
+    #fields = ("title", "content", "category", "timestamp")
+
+
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Category)
