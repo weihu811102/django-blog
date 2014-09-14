645e86c78809127fab2c6162609b3360814ebf08
WeiHu <weihu120@gmail.com>
2014-09-14 20:07:31 +0800

    test page complete.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from blog.models import Blog, Category
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    #return HttpResponse("Hello, world")
+    blogs = Blog.objects.all()
+    return render(request, "index.html", locals())
